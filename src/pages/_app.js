9d2795300405603cc25fe4f7acbac6d54abce1da
hjyang369 <ghlwls6@gmail.com>
2023-10-27 17:59:54 +0900

    add: 작성 페이지 레이아웃 완성

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,5 @@
 import "../styles/globals.css";
+import "../styles/normalize.css";
 
 const App = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import { Inter } from "next/font/google";
+import style from "../styles/main.module.css";
 //
 
 const inter = Inter({ subsets: ["latin"] });
@@ -13,7 +14,7 @@ const Main = () => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className={style.main}></div>
+      <div className={style.main}>main 입니다</div>
     </>
   );
 };
