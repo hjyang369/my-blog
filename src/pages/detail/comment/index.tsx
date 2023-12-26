1a2d9818af33b3cbc71804d637a28232b8481c3b
hjyang369 <ghlwls6@gmail.com>
2023-12-26 17:10:49 +0900

    fix: comment 컴포넌트 키 값 변경

--- src/pages/detail/comment/index.tsx
+++ src/pages/detail/comment/index.tsx
@@ -15,14 +15,14 @@ export default function Comment({ comment }: commentProps) {
       <div className={style.topContainer}>
         <div className={style.buttons}>
           <p className={style.name}>{comment?.name}</p>
-          <p>{comment.time}</p>
+          <p>{comment?.time}</p>
         </div>
         <div className={style.buttons}>
           <Button text={"수정"} fontSize={"1.2rem"} />
           <Button text={"삭제"} fontSize={"1.2rem"} />
         </div>
       </div>
-      <p className={style.text}>{comment.text}</p>
+      <p className={style.text}>{comment?.text}</p>
     </div>
   );
 }
