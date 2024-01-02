ec6281588a9375d79e15e833c70fa4b55346560c
hjyang369 <ghlwls6@gmail.com>
2024-01-02 10:38:00 +0900

    feat: 네브바 아이콘 변경

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -3,19 +3,35 @@ import style from "./nav.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPenToSquare, faUser } from "@fortawesome/free-solid-svg-icons";
 import { useRouter } from "next/router";
+import IC_Search from "../../../public/icon/Search";
+import { useState } from "react";
 
 export default function Nav({ onclick }) {
   const router = useRouter();
+  const [currentTab, setCurrentTab] = useState("이력서");
 
   const moveToPage = (paths) => {
     router.push(paths);
   };
 
+  const handleCurrentTab = () => {
+    if (currentTab === "블로그") {
+      setCurrentTab("이력서");
+      moveToPage("/");
+    } else {
+      setCurrentTab("블로그");
+      moveToPage("/resume");
+    }
+  };
+
   return (
     <nav className={style.nav}>
-      <button className={style.btn}>
-        <FontAwesomeIcon className={style.imgIcon} icon={faUser} />
-      </button>
+      <div>
+        <button className={style.btn}>
+          <FontAwesomeIcon className={style.imgIcon} icon={faUser} />
+        </button>
+        <button onClick={handleCurrentTab}>{currentTab}</button>
+      </div>
       <button
         onClick={() => {
           moveToPage("/");
@@ -24,7 +40,11 @@ export default function Nav({ onclick }) {
       >
         MY BLOG
       </button>
+
       <div>
+        <button className="iconBtn">
+          <IC_Search width="3rem" height="3rem" color="#f0b31e" />
+        </button>
         <button className={style.btn}>
           <FontAwesomeIcon
             onClick={
